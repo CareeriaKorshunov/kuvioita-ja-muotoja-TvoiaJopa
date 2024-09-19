5511f9dd9c92c0d5dfc96b8b05ecdb6066f38f17
Aretemii Korshunov <56575740+TvoiaJopa@users.noreply.github.com>
2024-09-19 13:07:20 +0300

    Update Program.cs

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -5,7 +5,23 @@
         public static void Main(string[] args)
         {
             //Koodi tähän
-           
+             Console.Write("Anna luku: ");
+            int luku = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\n" + luku + luku + luku + luku + luku);
+            Console.WriteLine(luku + " " + luku + " " + luku);
+            Console.WriteLine("" + luku + luku + luku + luku + luku);
+
+            Console.Write("\nAnna toinen luku: ");
+            int luku2 = int.Parse(Console.ReadLine());
+            Console.Write("Anna vielä kolmas luku: ");
+            int luku3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\n" + luku + " " + luku2 + " " + luku3);
+            Console.WriteLine(luku);
+            Console.WriteLine(luku2);
+            Console.WriteLine(luku3);
+            Console.WriteLine("" + luku3 + " " + luku2 + " " + luku);
         }
 
     }
